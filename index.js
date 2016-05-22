892e674dbc43cc8180bf9f8150f593a3265b018b
pvan06 <pvan06@hotmail.com>
2016-05-22 16:50:01 -0400

    Concatenate triangle string

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let sizeElement = document.getElementById("size");
+let poundsElement = document.getElementById("pounds");
 
 let drawTriangle = function(numLines) {
   console.log("new attempt");
   let numTry = 1
   let numPrint = "#"
+  let numPrintLn = "#\n"
   if (numLines < 1) {
 
     console.log("error, must be greater than zero")
@@ -13,8 +15,11 @@ let drawTriangle = function(numLines) {
       console.log(numPrint)
       numTry = numTry + 1
       numPrint = numPrint+"#"
+      numPrintLn = "#"+numPrintLn
     }
   }
+  
+  console.log(numPrintLn);
 }
 
 sizeElement.onchange = function() {
