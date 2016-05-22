1f118f7a57bc04261418e5db89d126f6531c2a06
pvan06 <pvan06@hotmail.com>
2016-05-22 16:38:59 -0400

    Add drawTriangle

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+let sizeElement = document.getElementById("size");
+let size = parseInt(sizeElement.value);
+
+let drawTriangle = function(numLines) {
+  console.log("new attempt");
+  let numTry = 1
+  let numPrint = "#"
+  if (numLines < 1) {
+
+    console.log("error, must be greater than zero")
+  } else {
+
+    while (numTry <= numLines) {
+      console.log(numPrint)
+      numTry = numTry + 1
+      numPrint = numPrint+"#"
+    }
+  }
+}
+drawTriangle(size);
